9aa46b9e8bdea0dbc848dee7ba8e4e12723802fd
Kravtsov <kravtsovofficial99@gmail.com>
2022-09-20 14:30:58 +0300

    правки

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,7 +1,7 @@
 
 import css from './FriendList.module.css'
 import  FriendListItem from './FriendListItem/FriendListItem'
-// import PropTypes from 'prop-types';
+
 
 const FriendList = ({ friends }) => {
     return (
@@ -20,16 +20,7 @@ const FriendList = ({ friends }) => {
     )
 }
 
-// FriendList.protoTypes = {
-//     friends: PropTypes.arrayOf(
-//         PropTypes.shape({
-//             id: PropTypes.number.isRequired,
-//             avatar: PropTypes.string.isRequired,
-//             name: PropTypes.string.isRequired,
-//             isOnline: PropTypes.bool.isRequired
-//         })
-//     ).isRequired,
-// };
+
 
 export default FriendList
 
